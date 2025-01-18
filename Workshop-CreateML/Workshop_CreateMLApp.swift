ece4b20a84d13fd5295a57a7a68d5103cbeaa6e5
Adolfo David Romero <Romeroad@Sheridancollege.ca>
2025-01-18 00:21:34 -0500

    Initial Commit

---
+++ Workshop-CreateML/Workshop_CreateMLApp.swift
@@ -0,0 +1,17 @@
+//
+//  Workshop_CreateMLApp.swift
+//  Workshop-CreateML
+//
+//  Created by David Romero on 2025-01-18.
+//
+
+import SwiftUI
+
+@main
+struct Workshop_CreateMLApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
